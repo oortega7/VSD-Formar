cd4d558b4beecedccd17a489c4351d3609556936
oortega7 <isaa777x1@gmail.com>
2025-05-14 00:04:26 +0000

    criação de usuário

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import {sequelize} from "./config/database";
+import User from "./models/user";
 
 const app = express();
 const port = 3000;
@@ -7,4 +9,20 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
+
+
+
+
+main()
+
+async function main() {
+  if (require.main === module) {
+    await sequelize.sync({ force: true }).then(() => {
+      console.log(`Listening at http://localhost:${port}`)
+  });
+};
+
+// Criar usuário
+const user = await User.create({ name: 'Alice', email: 'alice@email.com' });
+console.log("Usuário criado:", user.toJSON());
+}
